98f69c59f0cfd5015f3541e6af569028c162cb54
Xx-Guinoala-xX <37059120+Guinoala@users.noreply.github.com>
2018-03-21 10:54:17 -0700

    egg
    egg

--- bot.js
+++ bot.js
@@ -1,3 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require("./config.json");
+
+client.on('ready', () => {
+    client.user.setUsername('RavenBot');
+    console.log('The Raven has arrived.');
+});
+
+client.on("guildCreate", guild => {
+    console.log(`I joined a new server: ${guild.name} (id: ${guild.id}). This server has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
